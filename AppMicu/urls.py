120a13d863d004923b8adbab37e3f280a24b5c17
Sergio García <sergiosg88@gmail.com>
2024-09-06 23:08:24 -0300

    Se agrega video y testing

--- AppMicu/urls.py
+++ AppMicu/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('agregar-cliente/',views.cliente_form, name='agregar cliente'),
     path('agregar-producto/',views.ProductoCreateView.as_view(), name='New'),
     path('productos-cargados/',views.ProductoListView.as_view(), name='List'),
-    path('clases/detalle/<int:pk>/', views.ProductoDetalle.as_view(), name='Detail'),
-    path('clases/editar/<int:pk>', views.ProductoUpdateView.as_view(), name='Edit'),
-    path('clases/eliminar/<int:pk>', views.ProductoDeleteView.as_view(), name='Delete'),
+    path('detalle-producto/<int:pk>/', views.ProductoDetalle.as_view(), name='Detail'),
+    path('editar-producto/<int:pk>', views.ProductoUpdateView.as_view(), name='Edit'),
+    path('eliminar-producto/<int:pk>', views.ProductoDeleteView.as_view(), name='Delete'),
     path('comentarios/', views.comentario_form, name='comentarios'),
     path('sobrenosotros/', views.nosotros, name='nosotros')
 ]
